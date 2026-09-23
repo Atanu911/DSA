0c4429b130fc107d722a7418946913e75f2863cb
Atanu <ratanu4u@gmail.com>
2026-09-23 20:22:51 +0530

    sum of all substring

--- Strings/sum_of_all_substring_of_a_number_method_2.cpp
+++ Strings/sum_of_all_substring_of_a_number_method_2.cpp
@@ -1,13 +1,19 @@
 #include<iostream>
 #include<string>
 using namespace std;
-int main(){
-    string s = "926";
-    int n = s.length();
-    long long total_sum = 0;
+long long sumOfSubstrings(string s){
+    long long total_sum =0;
     long long current_sum = 0;
-    for(int i = 0;i<n;i++){
-        int digit = s[i]-'0';
+    for (int i = 0;i<s.length();i++){
+        int digit  = s[i]-'0';
+        current_sum = current_sum *10 +digit *(i+1);
+        total_sum += current_sum;
+
     }
+    return total_sum;
 }
-37:00 31lecctue
+int main() {
+    string s = "926";
+    cout<<"Sum of substrings:"<<sumOfSubstrings(s)<<endl;
+    return 0;
+}
